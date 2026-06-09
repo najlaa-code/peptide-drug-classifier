146d0855f39d58c341b2aa01de8e2e49ba735928
Najlaa <najlaa.achouhal@gmail.com>
2026-06-08 22:27:17 -0400

    Update command in pipeline_A_scaling.py docstring

--- pipelineA/pipeline_A_scaling.py
+++ pipelineA/pipeline_A_scaling.py
@@ -1,6 +1,6 @@
 """Feature Selection
 fitting std scaler only on training set, as part of Pipeline A
-ran with this cmd: python3 feature_selection_script_2.py -i features_raw_prepped_A.csv -o features_scaled_pipeline_A.csv
+ran with this cmd: python3 pipeline_A_scaling.py -i features_raw_prepped_A.csv -o features_scaled_pipeline_A.csv
 """
 import sys
 import argparse
