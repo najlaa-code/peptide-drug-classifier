85b8165f7089a00752a674368d1d918443c49331
Najlaa <najlaa.achouhal@gmail.com>
2026-08-09 23:38:12 -0400

    Add data splitting script for training and testing sets
    This script reads a CSV file, splits the data into training and testing sets based on a specified test size, and saves the split data to a new CSV file.

---
+++ pipelineB/pipeline_B_step0_split.py
@@ -0,0 +1,24 @@
+import argparse
+import pandas as pd
+from sklearn.model_selection import train_test_split
+def main(input_path, output_path, test_size, random_state):
+    df = pd.read_csv(input_path)
+    # stratify = None # if we do a random split
+    stratify = df["Class"] if "Class" in df.columns and df["Class"].nunique() < 20 else None #stratify: to preserve the class proportions relative to the class imbalance
+    train_idx, test_idx = train_test_split(
+        df.index, test_size=test_size, random_state=random_state, stratify=stratify
+    )
+    df["Split"] = "train"
+    df.loc[test_idx, "Split"] = "test"
+    print(f"train: {(df['Split'] == 'train').sum()}, test: {(df['Split'] == 'test').sum()}")
+    df.to_csv(output_path, index=False)
+    print(f"File saved in this path: {output_path}")
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("--input", "-i", default="features_scaled_pipeline_B.csv")
+    p.add_argument("--output", "-o", default="features_scaled_pipeline_B_split.csv")
+    p.add_argument("--test_size", type=float, default=0.2)
+    p.add_argument("--random_state", type=int, default=42)
+    args = p.parse_args()
+    main(args.input, args.output, args.test_size, args.random_state)
